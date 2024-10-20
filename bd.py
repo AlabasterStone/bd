3805971b778fd5b09ab196740d08205841f2ed2e
tkv <thewavylines@outlook.com>
2024-10-20 08:39:44 +0000

    upd

--- bd.py
+++ bd.py
@@ -8,7 +8,7 @@ video_url = st.text_input("url")
 res = requests.get(video_url, headers={"User-Agent":ua})
 match = re.search(r"video_url: '(.*?)', ", res.text, re.I|re.M)
 vid = match.group(1)
-res = requests.get(vid, headers={"User-Agent":ua}, allow_redirect=True)
+res = requests.get(vid, headers={"User-Agent":ua}, allow_redirects=True)
 new_url = res.url
 st.code(new_url, language="html")
 res.close()
