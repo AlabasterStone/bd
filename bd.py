ae5eeb1ffa45dea9158abcc438ebe1a4e4ccdd96
tkv <thewavylines@outlook.com>
2024-10-20 16:17:57 +0800

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import requests
+import re
 
 ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
 
 video_url = st.text_input("url")
 res = requests.get(video_url, headers={"User-Agent":ua})
-st.code(res.text, language="html")
+match = re.search(r"video_url: '(.*)'", res.text, re.I|re.M)
+st.code(match.group(1), language="html")
+res.close()
