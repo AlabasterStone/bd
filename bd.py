8f4a3948569e0a983cfce495459a493880ecb871
tkv <thewavylines@outlook.com>
2024-10-20 16:30:30 +0800

    Update bd.py

--- bd.py
+++ bd.py
@@ -7,5 +7,8 @@ ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like 
 video_url = st.text_input("url")
 res = requests.get(video_url, headers={"User-Agent":ua})
 match = re.search(r"video_url: '(.*?)', ", res.text, re.I|re.M)
-st.code(match.group(1), language="html")
+vid = match.group(1)
+res = requests.get(vid, headers={"User-Agent":ua}, allow_redirect=True)
+new_url = res.url
+st.code(new_url, language="html")
 res.close()
